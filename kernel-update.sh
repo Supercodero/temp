4943ad354c911ef7daf97ae8db06f5870881f984
Skyer <dlm-du@163.com>
2022-06-16 16:28:29 +0800

    Create kernel-update.sh

---
+++ kernel-update.sh
@@ -0,0 +1,108 @@
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y grub2-tools-extra grub2-tools-minimal grub2 grub2-pc-modules grub2-common grub2-pc grub2-tools
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y openssl-libs openssl
+sudo yum update -y iwl2000-firmware iwl105-firmware iwl2030-firmware iwl5000-firmware iwl4965-firmware iwl100-firmware iwl6000-firmware iwl6050-firmware linux-firmware iwl5150-firmware iwl135-firmware iwl7260-firmware iwl7265-firmware iwl1000-firmware iwl3945-firmware iwl6000g2a-firmware iwl3160-firmware iwl6000g2b-firmware
+sudo yum update -y procps-ng
+sudo yum update -y krb5-libs
+sudo yum update -y binutils
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y python-libs python
+sudo yum update -y elfutils-default-yama-scope elfutils-libelf elfutils-libs
+sudo yum update -y openssl-libs openssl
+sudo yum update -y libxml2
+sudo yum update -y gettext gettext-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y firefox
+sudo yum update -y firefox
+sudo yum update -y sqlite
+sudo yum update -y bash
+sudo yum update -y sudo
+sudo yum update -y rpm rpm-python rpm-build-libs rpm-libs
+sudo yum update -y mariadb-libs
+sudo yum update -y dhcp-common dhcp-libs dhclient
+sudo yum update -y openldap
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y python-libs python
+sudo yum update -y firefox
+sudo yum update -y openssl-libs openssl
+sudo yum update -y firefox
+sudo yum update -y cyrus-sasl-lib
+sudo yum update -y expat
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y openssl-libs openssl
+sudo yum update -y dhcp-common dhcp-libs dhclient
+sudo yum update -y python-libs python
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y NetworkManager-tui NetworkManager NetworkManager-libnm NetworkManager-team
+sudo yum update -y cpio
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y libpng
+sudo yum update -y binutils
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y vim-minimal
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y wpa_supplicant
+sudo yum update -y firefox
+sudo yum update -y libxml2
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y openssl-libs openssl
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y python-libs python
+sudo yum update -y rsyslog
+sudo yum update -y firefox
+sudo yum update -y openssh openssh-server openssh-clients
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y nss nss-sysinit nss-tools
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y microcode_ctl
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y microcode_ctl
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y openldap
+sudo yum update -y expat
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y zlib zlib-devel
+sudo yum update -y gzip
+sudo yum update -y microcode_ctl
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y freetype
+sudo yum update -y libcroco
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y file-libs file
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y e2fsprogs-libs libcom_err e2fsprogs libss
+sudo yum update -y python-libs python
+sudo yum update -y mariadb-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y python-libs python
+sudo yum update -y sudo
+sudo yum update -y shared-mime-info
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y rsyslog
+sudo yum update -y iwl2000-firmware iwl105-firmware iwl2030-firmware iwl5000-firmware iwl4965-firmware iwl100-firmware iwl6000-firmware iwl6050-firmware linux-firmware iwl5150-firmware iwl135-firmware iwl7260-firmware iwl1000-firmware iwl3945-firmware iwl6000g2a-firmware iwl3160-firmware iwl6000g2b-firmware
+sudo yum update -y sudo
+sudo yum update -y mariadb-libs
+sudo yum update -y expat
+sudo yum update -y libxml2
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y firefox
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
+sudo yum update -y NetworkManager-tui NetworkManager NetworkManager-libnm NetworkManager-team
+sudo yum update -y bind-libs-lite bind-license
+sudo yum update -y kernel-tools python-perf kernel kernel-tools-libs
